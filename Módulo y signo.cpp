f0dc67e5306197ca8f6f01b9073b51861316e419
MathiasLOL23 <marmas@alumno.montepiedra.edu.ec>
2025-10-13 13:05:14 -0500

    Módulo y signo.cpp

---
+++ Módulo y signo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 17;
+    int b = 5;
+
+    int resultado = a % b;  // Resultado: 17 dividido por 5 da 3, con resto 2
+
+    cout << "17 % 5 = " << resultado << endl;  // Imprime 2
+
+    return 0;
+}
