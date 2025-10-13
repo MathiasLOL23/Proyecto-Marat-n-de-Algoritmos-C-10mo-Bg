b040e91ee19400a2373faeba3b8314a59f5506b5
MathiasLOL23 <marmas@alumno.montepiedra.edu.ec>
2025-10-13 13:29:30 -0500

    Ordenar N números (bubble sort).cpp

---
+++ Ordenar N números (bubble sort).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cout << "Ingrese la cantidad de números: ";
+    cin >> N;
+
+    int arr[N];
+
+    // Leer los números
+    cout << "Ingrese los " << N << " números:\n";
+    for (int i = 0; i < N; ++i) {
+        ci
